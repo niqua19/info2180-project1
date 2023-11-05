7d4eebb622746eac05c68c2c986a4493f5e461b3
Sheniqua Johnson <sheniquajohnson68@gmail.com>
2023-11-04 23:05:55 -0500

    js implemented

--- scripts.js
+++ scripts.js
@@ -1,21 +1,19 @@
 /* Add your JavaScript to this file */
 
-window.onload = loadPage;
-
-function loadPage(){
-    let submit = document.querySelectorAll(".btn")[1];
+window.onload = function(){
+    var button = document.querySelectorAll(".btn")[1];
         
-    submit.addEventListener('click', clicker);
+    button.addEventListener('click', clicker);
 }
 
 function clicker(event){
     event.preventDefault();
 
-    var emailAddress = document.getElementById("email").value;
+    var email = document.getElementById("email").value;
     // alert(emailAddress)
 
-    if(emailAddress != ""){
-        document.getElementsByClassName("message")[0].innerHTML = "Thank You! Your email address " +emailAddress+ " has been added to our mailing list";
+    if(email != ""){
+        document.getElementsByClassName("message")[0].innerHTML = "Thank You! Your email address " +email+ " has been added to our mailing list";
     }else{
         document.getElementsByClassName("message")[0].innerHTML = "please enter your email address";
     }
