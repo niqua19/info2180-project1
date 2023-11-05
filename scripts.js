0579a530403686b65c5e0b4d58d46c6c395061b3
Sheniqua Johnson <sheniquajohnson68@gmail.com>
2023-11-04 22:57:10 -0500

    js part

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+
+window.onload = loadPage;
+
+function loadPage(){
+    let submit = document.querySelectorAll(".btn")[1];
+        
+    submit.addEventListener('click', clicker);
+}
+
+function clicker(event){
+    event.preventDefault();
+
+    var emailAddress = document.getElementById("email").value;
+    // alert(emailAddress)
+
+    if(emailAddress != ""){
+        document.getElementsByClassName("message")[0].innerHTML = "Thank You! Your email address " +emailAddress+ " has been added to our mailing list";
+    }else{
+        document.getElementsByClassName("message")[0].innerHTML = "please enter your email address";
+    }
+}
